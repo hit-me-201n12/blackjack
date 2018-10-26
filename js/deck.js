04044bafe93336c0089bee6829e7a5776b85fbea
Karl Polintan <kf.polintan@gmail.com>
2018-10-25 19:20:54 -0700

    52 card pickup

--- js/deck.js
+++ js/deck.js
@@ -3,7 +3,7 @@
 function Card (value, suit) {
     this.value = value;
     this.suit = suit;
-    this.face = down;
+    this.face = 'down';
   //point value as another property?
 };
 
@@ -12,20 +12,23 @@ function Card (value, suit) {
 
 
 function Deck () {
-    inPlay = [];
-    disCard = [];
+    this.inPlay = [];
+    this.disCard = [];
 };
 
 //Deck.prototype.shuffle() -- shuffles deck
 //Deck.prototype.deal() -- deals cards
 
 Deck.prototype.build = function() {//-- uses card constructor to build new deck
-  var suits = ['diamond', 'clubs', 'hearts', 'spades'];
-  var values = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
+  var suit = ['diamond', 'clubs', 'hearts', 'spades'];
+  var value = ['2','3','4','5','6','7','8','9','10','Jack','Queen','King','Ace'];
 
-  for (var i = 0; i<suits.length; i++){
-    for (var j = 0; j<values.length; j++){
-      this.inPlay.push(card(j,i));
+  for (var i = 0; i<suit.length; i++){
+    for (var j = 0; j<value.length; j++){
+      var card = new Card(value[j],suit[i]);
+      this.inPlay.push(card);
+      console.log(j);
     }
+    console.log(i);
   }
 }
