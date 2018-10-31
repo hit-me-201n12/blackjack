7a070e7788f359efc6bc381e285e074b9d39f523
Michael Lyon <lyoncodes@gmail.com>
2018-10-30 21:45:04 -0700

    adds pop up to left side of game board. adds about uswireframe with basic color scheme. Needs img for wire frame to render proper. added aboutUs.js

--- js/table.js
+++ js/table.js
@@ -1,5 +1,4 @@
 'use strict';
-let log = console.log();
 
 // Purpose:
 // Starting Game
@@ -22,25 +21,11 @@ players.push(new Player('Dealer', true));
 //Create deck for the game
 var deck;
 
-var setOrder = function(){
-  for (var i=0 ; i<players.length-1 ; i++){
-    players[i].order=i+1;
-  }
-  players[players.length-1].order=0;
-};
-
 var eventhandler = function(press) { //this is our event handler for hitting and staying.
-  
-  console.log(current);
-  if (current===-1){
-    return;
-  }
-  //condition, only use keyboard for hit and stay if not the dealer
-  if (players[current].playing){//condition, only hit or stay if the current player is still playing
-    if (!players[current].dealer===true){
+  if (!players[current].dealer===true){//condition, only use keyboard for hit and stay if not the dealer
+    if (players[current].playing){//condition, only hit or stay if the current player is still playing
       let key = press.char || press.charCode || press.which;
       if (key === 32) { //if the user presses space:
-        press.preventDefault();
         players[current].hit();//player is dealt a card
         // Check to see if player has busted or blackJack to set 'playing' to false
         if(players[current].blackJack){
@@ -51,13 +36,13 @@ var eventhandler = function(press) { //this is our event handler for hitting and
           nextPlayer();
         }
       } else if (key === 13) {// if user presses enter
-        // function call back playerStand()
-        press.preventDefault();
+      // function call back playerStand()
         players[current].stay();
         nextPlayer();
       }
     }
   }
+
 };
 
 
@@ -68,10 +53,8 @@ var eventhandler = function(press) { //this is our event handler for hitting and
 var gamePlay = function() {
   //Write something to clear/reset all players objects (hand, and booleans)
   newRound();
-  console.log('newRound');
   // deal the cards
   dealcards();
-  console.log('dealCards');
   // taking turns checking scores
   nextPlayer();
 };
@@ -83,130 +66,88 @@ var newRound = function (){
   for (var i in players){
     players[i].newGame;
   }
-  setOrder();
 };
 
 var dealcards = function(){
   for(var j = players.length-1; j > -1; j--){
     players[j].hit();
+    console.log(players[j]);
   }
   for(var i in players) {
     players[i].hit();
+    console.log(players[i]);
   }
 };
 
 var nextPlayer = function(){
   current++;
-  playerOrder(1);
-
-  console.log();
-  console.log(players[current].ID+'s turn, current score: '+players[current].hand.score+' order '+players[current].order);
+  console.log(players[current].ID+'s turn, current score: '+players[current].hand.score);
   //console.log(players[current]+'s turn, score: '+players[current].hand.score);
-
-
-
   if(players[current].dealer){//if the current player is the dealer
-    dealerTurn();
+    window.removeEventListener('keypress', window);
+    var dPlay=true;
+    while(dPlay===true){
+      if(players[current].hand.score<17){
+        players[current].hit();
+      }else if (players[current].hand.score === 17 && players[current].hand.aces>0){
+        players[current].hit;
+      }else if(players[current].hand.score===17){
+        console.log('dealerStay');
+        players[current].stay;
+        dPlay=false;
+        break;
+      }else{
+        console.log('dealerStay');
+        players[current].stay;
+        dPlay=false;
+        break;
+      }
+    }
+    current--;
+    checkScores();
   }else if (current<players.length-1){
     window.addEventListener('keypress', eventhandler);
     if(!players[current].playing && players[current].hand.blackJack){
       console.log(players[current].ID+' scored blackjack off of the draw!');
-      nextPlayer();
-    }
-  }
-};
-
-var dealerTurn = function(){
-  window.removeEventListener('keypress', window);
-  var dPlay=true;
-  while(dPlay===true){
-    if(players[current].hand.score<17){ //hits below 17
-      players[current].hit();
-      if(players[current].busted||players[current].hand.score===21){
-        dPlay=false;
-        players[current].playing=false;
-        checkScores();
-      }
-    }else if (players[current].hand.score === 17 && players[current].hand.aces>0){ //hits a soft 17
-      players[current].hit;
-      if(players[current].busted||players[current].hand.score===21){
-        dPlay=false;
-        players[current].playing=false;
-        checkScores();
-        return;
-      }
-    }else if(players[current].hand.score===17){
-      console.log('dealerStay');
-      players[current].stay;
-      players[current].playing=false;
-      checkScores();
-      dPlay=false;
-      return;
-    }else{
-      console.log('dealerStay');
-      players[current].stay;
-      players[current].playing=false;
-      checkScores();
-      dPlay=false;
-      return;
+      current++;
+    } else {
+      current=-1;
     }
   }
 };
 
 var checkScores = function(){
-  current--;
-  playerOrder(-1);
-  if (current>-1){
-    if (!players[players.length-1].playing){
-      var dealerbust= (players[players.length-1].busted);
-      var dealerScore = players[players.length-1].hand.score;
-      if (!players[current].busted){
-        if (players[current].hand.score>dealerScore||dealerbust){
-          players[current].wins++;
-          console.log(players[current].ID+' won their hand.');
-        }else if (players[current].hand.score===dealerScore){
-          players[current].wins+=0;
-          console.log(players[current].ID+' washed.');
-        }else{
-          players[current].wins--;
-          console.log(players[current].ID+' lost their hand');
-        }
+  console.log(current);
+  if (!players[players.length-1].playing){
+    var dealerbust= (players[players.length-1].busted);
+    var dealerScore = players[players.length-1].hand.score;
+    if (!players[current].bust){
+      if (players[current].hand.score>dealerScore||dealerbust){
+        players[current].wins++;
+        console.log(players[current]+' won their hand.');
+      }else if (players[current].hand.score===dealerScore){
+        players[current].wins+=0;
       }else{
-        console.log(players[current].ID+' lost their hand.');
+        players[current].wins--;
       }
-      if (current>-1){
-        checkScores();
-      }
+    }else{
+      console.log(players[current]+' lost their hand.');
+    }
+    if (current>-1){
+      checkScores();
     }
   }
 };
 
-var playerOrder = function(direction){
-  if (direction===1){
-    for(var i = 0; i<players.length-1; i++) {
-      if (players[i].order===1){
-        players[i].order===players.length;
-      }else{
-        players[i].order--;
-      }
-    }
-  }
-  if (direction===-1){
-    for(var i = 0; i<players.length-1; i++) {
-      if (players[i].order===players.length){
-        players[i].order===1;
-      }else{
-        players[i].order++;
-      }
-    }
-  }
-}
 
 var testGame = function() {
   console.log(players);
   gamePlay();
-
 };
-
+// pop up function
+var showRules = function () {
+  var popup = document.getElementById('popUp') // select the HTML div box & assign to 'popup'
+  popup.classList.toggle('appear') // call classList.toggle DOM method on popup element. This DOM method creates a class with the name of the string value passed as an argument. In the CSS, a selector named .popUpRules .appear exists already. When we create this class and toggle it on popup, it activates the CSS block that styles the popup. One of the properties is visibility: visible. So showRules() takes the element in its existing state of visibility: hidden to the new state of visibility: visible. With a z-index, it appears on top of other elements.
+}
 // function calls
 testGame();
